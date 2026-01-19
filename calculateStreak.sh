3aa30d213cdd976bbdd71207bd3ea7af14759519
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-19 17:41:42 -0300

    Update calculateStreak.sh

--- calculateStreak.sh
+++ calculateStreak.sh
@@ -2,7 +2,7 @@ USERNAME=$1
 GITHUB_PAT=$2
 EVENTS=$(curl --location 'https://api.github.com/graphql' \
 --header 'Content-Type: application/json' \
---header 'Authorization: $GITHUB_PAT' \
+--header "Authorization: $GITHUB_PAT" \
 --data '{
     "query": "query { user(login: \"juancolchete\") { name createdAt contributionsCollection { startedAt contributionCalendar { totalContributions weeks { contributionDays { date contributionCount } } } } } }"
 }')
