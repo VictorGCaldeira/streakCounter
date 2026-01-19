563dad0e3bb0eb07300ed077a3c85f8c19c0705d
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-19 20:52:46 -0300

    ci: fix wrong  quoting

--- calculateStreak.sh
+++ calculateStreak.sh
@@ -11,7 +11,7 @@ EVENTS=$(curl --location 'https://api.github.com/graphql' \
 --header 'Content-Type: application/json' \
 --header "Authorization: Bearer $GITHUB_PAT" \
 --data '{
-    "query": "query { user(login: \"juancolchete\") { name createdAt contributionsCollection(from: "'"$ACCOUNT_CREATED_AT"'") { startedAt contributionCalendar { totalContributions weeks { contributionDays { date contributionCount } } } } } }"
+    "query": "query { user(login: \"juancolchete\") { name createdAt contributionsCollection(from: \"'"$ACCOUNT_CREATED_AT"'\") { startedAt contributionCalendar { totalContributions weeks { contributionDays { date contributionCount } } } } } }"
 }')
 echo $EVENTS
 echo $USERNAME started at $ACCOUNT_CREATED_AT streak 9000
