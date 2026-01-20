21afac57e3c6965f08ee00d44242787ff3169976
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-19 21:10:28 -0300

    Update calculateStreak.sh

--- calculateStreak.sh
+++ calculateStreak.sh
@@ -7,11 +7,12 @@ EVENTS=$(curl --location 'https://api.github.com/graphql' \
     "query": "query { user(login: \"juancolchete\") { name createdAt  } }"
 }')
 ACCOUNT_CREATED_AT=$(echo $EVENTS | jq -r '.data.user.createdAt')
+ACCOUNT_CREATED_AT_YEAR=$(${ACCOUNT_CREATED_AT:0:10})
 EVENTS=$(curl --location 'https://api.github.com/graphql' \
 --header 'Content-Type: application/json' \
 --header "Authorization: Bearer $GITHUB_PAT" \
 --data '{
-    "query": "query { user(login: \"juancolchete\") { name createdAt contributionsCollection(from: \"'$ACCOUNT_CREATED_AT'\") { startedAt contributionCalendar { totalContributions weeks { contributionDays { date contributionCount } } } } } }"
+    "query": "query { user(login: \"juancolchete\") { name createdAt contributionsCollection(from: \"'$ACCOUNT_CREATED_AT'T00:00:00Z\") { startedAt contributionCalendar { totalContributions weeks { contributionDays { date contributionCount } } } } } }"
 }')
 echo $EVENTS
 echo $USERNAME started at $ACCOUNT_CREATED_AT streak 9000
