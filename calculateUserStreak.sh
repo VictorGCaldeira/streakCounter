ff9c334e40b435c6fd6255e142b408bbf4ec8bbb
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-19 22:57:39 -0300

    Update calculateUserStreak.sh

--- calculateUserStreak.sh
+++ calculateUserStreak.sh
@@ -3,9 +3,9 @@ STREAK_COUNT=0
 TODAY=$(date -u +"%Y-%m-%d" -d "-3 hours")
 cat "contributions/${USERNAME}.json" | jq '.[] | [select(.date < "'$TODAY'")] | .[].contributionCount' | while read -r count; 
 do 
-  echo $count
   if [[ $count -gt 0 ]]; then
     STREAK_COUNT=$(( STREAK_COUNT + 1 ))
+    echo $STREAK_COUNT
   else
     STREAK_COUNT=0
   fi
