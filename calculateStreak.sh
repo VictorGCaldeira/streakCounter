f0d86b364fcdb15a83fa7af3390fab0b5cce8ac7
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-19 21:12:50 -0300

    Update calculateStreak.sh

--- calculateStreak.sh
+++ calculateStreak.sh
@@ -8,6 +8,8 @@ EVENTS=$(curl --location 'https://api.github.com/graphql' \
 }')
 ACCOUNT_CREATED_AT=$(echo $EVENTS | jq -r '.data.user.createdAt')
 ACCOUNT_CREATED_AT_YEAR=$(${ACCOUNT_CREATED_AT:0:10})
+echo "acccvre"
+echo $ACCOUNT_CREATED_AT_YEAR
 EVENTS=$(curl --location 'https://api.github.com/graphql' \
 --header 'Content-Type: application/json' \
 --header "Authorization: Bearer $GITHUB_PAT" \
