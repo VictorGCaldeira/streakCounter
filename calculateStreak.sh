ec94ea754871d300c7f845c50a66e0c749d9bcf7
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-19 21:15:30 -0300

    Update calculateStreak.sh

--- calculateStreak.sh
+++ calculateStreak.sh
@@ -7,7 +7,7 @@ EVENTS=$(curl --location 'https://api.github.com/graphql' \
     "query": "query { user(login: \"juancolchete\") { name createdAt  } }"
 }')
 ACCOUNT_CREATED_AT=$(echo $EVENTS | jq -r '.data.user.createdAt')
-ACCOUNT_CREATED_AT_YEAR=$(${ACCOUNT_CREATED_AT:0:4})
+ACCOUNT_CREATED_AT_YEAR=${ACCOUNT_CREATED_AT:0:4}
 echo "acccvre"
 echo "$ACCOUNT_CREATED_AT_YEAR"
 EVENTS=$(curl --location 'https://api.github.com/graphql' \
