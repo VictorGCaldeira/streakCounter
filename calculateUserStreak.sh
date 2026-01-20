9553d0a2c5b96e890124d53dd10c2ae86a5e8df4
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-20 14:00:02 -0300

    ci: contribution count

--- calculateUserStreak.sh
+++ calculateUserStreak.sh
@@ -3,6 +3,7 @@ STREAK_COUNT=0
 CONTRIBUTION_COUNT=0
 
 TODAY=$(date -u +"%Y-%m-%d" -d "-3 hours")
+CONTRIBUTION_DAYS_COUNT=$(cat "contributions/${USERNAME}.json" | jq -r '.[] | select(.date < "'$TODAY'") | .length')
 while read -r count; 
 do 
   if [[ $count -gt 0 ]]; then
@@ -12,5 +13,6 @@ do
     STREAK_COUNT=0
   fi
 done < <(jq -r '.[] | select(.date < "'$TODAY'") | .contributionCount' "contributions/${USERNAME}.json")
-AVG_CONTRIBUTION=$((CONTRIBUTION_COUNT / $(cat "contributions/${USERNAME}.json" | jq -r '.[] | select(.date < "'$TODAY'") | .length') ))
+echo contr $CONTRIBUTION_DAYS_COUNT
+AVG_CONTRIBUTION=$((CONTRIBUTION_COUNT / $CONTRIBUTION_DAYS_COUNT ))
 echo $USERNAME streak is $STREAK_COUNT total contributions $CONTRIBUTION_COUNT avg contributions per day $AVG_CONTRIBUTION
