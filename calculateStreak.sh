f7d6282c214dda95f843d07995e12b7e617426be
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-19 21:46:07 -0300

    Update calculateStreak.sh

--- calculateStreak.sh
+++ calculateStreak.sh
@@ -12,12 +12,17 @@ ACCOUNT_CREATED_AT_YEAR=${ACCOUNT_CREATED_AT:0:4}
 YEARS_TO_RUN=$((TODAY_YEAR - ACCOUNT_CREATED_AT_YEAR))
 echo "acccvre"
 echo "$YEARS_TO_RUN"
-EVENTS=$(curl --location 'https://api.github.com/graphql' \
---header 'Content-Type: application/json' \
---header "Authorization: Bearer $GITHUB_PAT" \
---data '{
-    "query": "query { user(login: \"juancolchete\") { name createdAt contributionsCollection(from: \"'$ACCOUNT_CREATED_AT_YEAR'-01-01T00:00:00Z\") { startedAt contributionCalendar { totalContributions weeks { contributionDays { date contributionCount } } } } } }"
-}')
-echo $EVENTS | jq '[.data.user.contributionsCollection.contributionCalendar.weeks[].contributionDays[]] | .[:-1]' > "$USERNAMEContributions.json"
-echo $USERNAME started at $ACCOUNT_CREATED_AT streak 9000
-cat "$USERNAMEContributions.json"
+RUN_YEAR=$ACCOUNT_CREATED_AT_YEAR
+for i in $(seq 0 $YEARS_TO_RUN)
+do
+    EVENTS=$(curl --location 'https://api.github.com/graphql' \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $GITHUB_PAT" \
+    --data '{
+        "query": "query { user(login: \"juancolchete\") { name createdAt contributionsCollection(from: \"'$ACCOUNT_CREATED_AT_YEAR'-01-01T00:00:00Z\") { startedAt contributionCalendar { totalContributions weeks { contributionDays { date contributionCount } } } } } }"
+    }')
+    echo $EVENTS | jq '[.data.user.contributionsCollection.contributionCalendar.weeks[].contributionDays[]] | .[:-1]' > "$USERNAMEContributions.json"
+    echo $USERNAME started at $ACCOUNT_CREATED_AT streak 9000
+    cat "$USERNAMEContributions.json"
+    RUN_YEAR=$((ACCOUNT_CREATED_AT_YEAR + 1))
+done
