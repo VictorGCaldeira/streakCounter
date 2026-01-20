f8356cd03ac502a03ba57900e440d33f8874e1ab
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-20 13:58:10 -0300

    ci: fix not getting file

--- calculateUserStreak.sh
+++ calculateUserStreak.sh
@@ -12,5 +12,5 @@ do
     STREAK_COUNT=0
   fi
 done < <(jq -r '.[] | select(.date < "'$TODAY'") | .contributionCount' "contributions/${USERNAME}.json")
-AVG_CONTRIBUTION=$((CONTRIBUTION_COUNT / $(jq -r '.[] | select(.date < "'$TODAY'") | .length') ))
+AVG_CONTRIBUTION=$((CONTRIBUTION_COUNT / $(cat "contributions/${USERNAME}.json" | jq -r '.[] | select(.date < "'$TODAY'") | .length') ))
 echo $USERNAME streak is $STREAK_COUNT total contributions $CONTRIBUTION_COUNT avg contributions per day $AVG_CONTRIBUTION
