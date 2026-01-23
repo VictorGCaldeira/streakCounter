ddb0f2cdb94f82e8cdcea75f7574f08034149a05
juancolchete <juanudkp@gmail.com>
2026-01-23 10:52:14 -0300

    fix: contributions directory savement and creation

--- saveContributionCountPerDay.sh
+++ saveContributionCountPerDay.sh
@@ -12,7 +12,7 @@ ACCOUNT_CREATED_AT=$(echo $EVENTS | jq -r '.data.user.createdAt')
 ACCOUNT_CREATED_AT_YEAR=${ACCOUNT_CREATED_AT:0:4}
 USER_DATA=$(echo $EVENTS | jq '.data')
 RUN_YEAR=$ACCOUNT_CREATED_AT_YEAR
-mkdir -p "${USERNAME}/cntributions"
+mkdir -p "${USERNAME}/contributions"
 mkdir -p data
 echo "[]" > "${USERNAME}/contributions/${USERNAME}.json"
 echo $USER_DATA > "data/${USERNAME}.json"
@@ -27,7 +27,7 @@ if [[ $ACCOUNT_CREATED_AT_YEAR -gt 1900 ]]; then
         }')
         NEW_DAYS=$(echo $RESPONSE | jq '[.data.user.contributionsCollection.contributionCalendar.weeks[].contributionDays[]] | .[:-1]')
         RUN_YEAR=$((RUN_YEAR + 1))
-        jq -n --slurpfile old "contributions/${USERNAME}.json" --argjson new "$NEW_DAYS" '($old | add) + $new' > "temp.json" && mv "temp.json" "${USERNAME}/contributions/${USERNAME}.json"
+        jq -n --slurpfile old "${USERNAME}/contributions/${USERNAME}.json" --argjson new "$NEW_DAYS" '($old | add) + $new' > "temp.json" && mv "temp.json" "${USERNAME}/contributions/${USERNAME}.json"
     done
 else
 echo account $USERNAME does not exists on github
